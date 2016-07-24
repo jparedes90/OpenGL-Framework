7a17d8e1287ede97d4046039b829b1e2284ea6c0
jparedes90 <jparedes90@users.noreply.github.com>
2016-07-24 08:04:21 -0700

    Create input_manager.h

---
+++ OpenGL_4_0_Engine/src/inputs/input_manager.h
@@ -0,0 +1,47 @@
+#pragma once
+#ifndef _INPUT_MANAGER_H
+#define _INPUT_MANAGER_H
+#include "KeyBoardState.h"
+#include "MouseState.h"
+#include "../MathCore.h"
+
+class input_manager
+{
+private:
+	KeyBoardState m_curKeyBoardState;
+	KeyBoardState m_prevKeyBoardState;
+	MouseState m_currentMouseState;
+	MouseState m_prevMouseState;
+	Vector2 m_mousePosition;
+	Vector2 m_prevMousePosition;
+
+public:
+	input_manager(void);
+	~input_manager(void);
+
+	void Update();
+
+	bool IsKeyDown(KeyPad::Keys key);
+	bool IsKeyUp(KeyPad::Keys key);
+	bool IsKeyHit(KeyPad::Keys key);
+	bool IsKeyReleased(KeyPad::Keys key);
+
+	bool IsButtonDown(Mouse::MousePad mouse);
+	bool IsButtonUp(Mouse::MousePad mouse);
+	bool IsButtonHit(Mouse::MousePad mouse);
+	bool IsButtonReleased(Mouse::MousePad mouse);
+
+	const Vector2& getMousePosition();
+	const Vector2& getPrevMousePosition();
+
+	void EventHandlerKeyDown(WPARAM wParam);
+	void EventHandlerKeyUp(WPARAM wParam);
+
+	void MouseEventLDown();
+	void MouseEventLUp();
+	void MouseEventRDown();
+	void MouseEventRUp();
+
+	void SetMouseOnScreen(bool mouseOnScreen, bool wrapArroundScreen);
+};
+#endif
