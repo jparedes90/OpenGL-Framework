19e186b2ee2f014bb54d57e65f7fb6a3ab95cae9
jparedes90 <jparedes90@users.noreply.github.com>
2016-07-24 08:06:03 -0700

    Create MouseState.h

---
+++ OpenGL_4_0_Engine/src/inputs/MouseState.h
@@ -0,0 +1,37 @@
+#pragma once
+#ifndef _MOUSE_STATE_H
+#define _MOUSE_STATE_H
+#include "Keys.h"
+#include "../MathCore.h"
+
+class MouseState
+{
+private:
+	bool mouseLButton;
+	bool mouseRButton;
+	bool m_keepMouseOnScreen;
+	bool m_wrapMouseAroundScreen;
+	POINT pt;
+	bool m_init;
+	Vector2 m_mousePosition;
+	Vector2 m_prevMousePosition;
+
+public:
+	MouseState(void);
+	~MouseState(void);
+
+	void Update();
+	bool IsButtonDown(Mouse::MousePad mouse);
+	bool IsButtonUp(Mouse::MousePad mouse);
+
+	const Vector2& getMousePosition();
+	const Vector2& getPrevMousePosition();
+
+	void MouseEventLDown();
+	void MouseEventLUp();
+	void MouseEventRDown();
+	void MouseEventRUp();
+
+	void SetMouseOnScreen(bool mouseOnScreen, bool wrapArroundScreen);
+};
+#endif
